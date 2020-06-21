23b4f1101ec68240e0fa61bbfc0bfec7005069c1
Akorus1 <Phiuris1@gmail.com>
2020-06-21 22:58:35 +0200

    Commit n° 5 by Kévin

---
+++ Accueil_et_Mentions_légales/Accueil.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <title>Acceuil</title>
+</head>
+<body>
+<header>
+        <?php 
+        /*require_once("../boutique/bdd.php");*/
+        require_once("../Menu_et_Footer/menu.php"); ?>
+</header>
+
+
+
+
+
+</body>
+<footer>
+        <?php require_once("../Menu_et_Footer/footer.php"); ?>
+</footer>
+</html>
---
+++ Menu_et_Footer/Footer.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="http://localhost/Projet-2-Veille-techno/Menu_et_Footer/Footer.css">
+<footer>
+<nav class="navbar fixed-bottom navbar-dark bg-dark">
+  <a class="navbar-brand" href="http://localhost/Projet-2-Veille-techno/Fonctionnalités_Site/contact.php">Nous contacter</a>
+  <a class="navbar-brand" href="http://localhost/Projet-2-Veille-techno/Accueil_et_Mentions_légales/MentionsLegales.php">Mentions légales</a>
+  <a class="navbar-brand" href="http://localhost/Projet-2-Veille-techno/Fonctionnalités_Site/conditions_générales_ventes.php">Conditions générales de ventes</a>
+</nav>
+</footer>
