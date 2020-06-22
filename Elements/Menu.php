2a090b073df0c0ee4a11ee1971034d0f67091c02
Akorus1 <Phiuris1@gmail.com>
2020-06-22 03:41:07 +0200

    Commit n°6 by Kevib

---
+++ Elements/Menu.php
@@ -0,0 +1,26 @@
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>   
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<nav class="navbar navbar-dark bg-dark">
+  <a class="navbar-brand" style="color: #FFFFFF";>Accueil</a>
+  <div class="dropdown">
+  <a class="btn btn-dark dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+  La boutique
+  </a>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <a class="dropdown-item"  href="http://localhost/Projet-2-Veille-techno/Fonctionnalités_Site/Inscription.php">Les masques</a>
+    <a class="dropdown-item"  href="http://localhost/Projet-2-Veille-techno/Fonctionnalités_Site/Inscription.php">Les parois anti-contact</a>
+    <a class="dropdown-item"  href="http://localhost/Projet-2-Veille-techno/Fonctionnalités_Site/Inscription.php">Les gants</a>
+    <a class="dropdown-item"  href="http://localhost/Projet-2-Veille-techno/Fonctionnalités_Site/Inscription.php">Passer une commande</a>
+  </div>
+</div>
+  <a class="btn btn-dark" href="http://localhost/Projet-2-Veille-techno/Fonctionnalités_Site/Lequipe.php" role="button">Qui sommes-nous ?</a>
+  <a class="btn btn-dark" href="http://localhost/Projet-2-Veille-techno/Fonctionnalités_Site/Notresponsor.php" role="button">Notre sponsor</a>
+  <a class="btn btn-dark" href="http://localhost/Projet-2-Veille-techno/Fonctionnalités_Site/Connexion.php" role="button">Se connecter</a>
+  <a class="btn btn-dark" href="http://localhost/Projet-2-Veille-techno/Fonctionnalités_Site/Inscription.php" role="button">S'inscrire</a>
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Chercher un produit" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">OK</button>
+  </form>
+</nav>
