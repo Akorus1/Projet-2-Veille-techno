d291ced67d35a7958160c0606dd916422cdad72f
Akorus1 <Phiuris1@gmail.com>
2020-06-22 22:53:28 +0200

    Commit n°7 by Kevin

---
+++ Fonctionnalités_Site/contact.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/Projet-2-Veille-techno/Elements/Style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <title>Contactez nous</title>
+</head>
+<header>
+        <?php 
+        /*require_once("../boutique/bdd.php");*/
+        require_once("../Elements/menu.php"); ?>
+</header>
+<body>
+ <div class="box">
+ <h1>Nous contacter</h1>
+ <p class="p1">Pour toutes questions éventuelle ou soucis technique veuillez prendre contact avec les admins ci-dessous</p>
+ <p class="p2">Meffodong Kévin <br />
+ Adresse mail: kevin.meffodong@viacesi.fr <br />
+ Numéro de téléphone: 0782499184 <br />
+</p>
+</div>
+</body>
+<footer>
+        <?php require_once("../Elements/footer.php"); ?>
+</footer>
+</html>
